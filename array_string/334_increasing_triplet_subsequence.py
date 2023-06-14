70d5f2456a52f26855c5602031dd50e7b2364a33
Valentin Simenov <simeonov2304@gmail.com>
2023-06-14 08:55:40 +0300

    Update

---
+++ array_string/334_increasing_triplet_subsequence.py
@@ -0,0 +1,11 @@
+class Solution:
+    def increasingTriplet(self, nums: list[int]) -> bool:
+        first = second = float('inf')
+        for n in nums:
+            if n <= first:
+                first = n
+            elif n <= second:
+                second = n
+            else:
+                return True
+        return False
