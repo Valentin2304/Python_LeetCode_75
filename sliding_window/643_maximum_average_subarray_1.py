ad9299cfb2839848389c21d7b7757c56f1bb1121
Valentin Simenov <simeonov2304@gmail.com>
2023-06-16 19:03:40 +0300

    Update

---
+++ sliding_window/643_maximum_average_subarray_1.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findMaxAverage(self, nums: list[int], k: int) -> float:
+        cur = result = sum(nums[0: k])
+        for i in range(len(nums) - k):
+            cur += nums[i + k] - nums[i]
+            result = max(cur, result)
+        return float(result) / k
+
+
